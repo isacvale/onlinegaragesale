2b4aa623609210df422d30c7becf22938898af78
Carol Arado <arado.carol@gmail.com>
2020-02-28 22:11:37 -0300

    Adiciona dados

---
+++ data.js
@@ -0,0 +1,165 @@
+export default [
+  {
+    type: 'CDs',
+    name: 'Elvis Presley',
+    short: 'A nice CD from the King.',
+    long: 'A very nice CD from the King.',
+    images: ['cd-elvis-01.jpg'],
+    price: 3,
+    status: null,
+    alias: 'cd_elvis_natal'
+  },
+  {
+    type: 'Video-game',
+    name: 'Wii U',
+    short: 'A googy goody Netflix player.',
+    long: 'It could play games occasionally.',
+    images: ['videogame-wiiu-01.jpeg'],
+    price: 300,
+    status: null,
+    alias: 'wii_u'
+  },
+
+  {
+    type: 'Livros',
+    name: 'A Grande Enciclopédia Ilustrada das Plantas e Flores',
+    short: 'Enciclopédia completa com 12 volumes.',
+    long: 'É a maior coleção de plantas ornamentais em língua portuguesa, dividida em 12 volumes, em 3.168 páginas e mais de 2.600 espécies.Biblioteca Natureza. Excelente estado com 2 volumes ainda lacrados.',
+    by: 'Romahn, Valerio.',
+    images: [''],
+    price: 250,
+    status: null,
+    alias: 'lv_encic_plantas'
+  },
+  {
+    type: 'Livros',
+    name: 'bebê - manual do proprietário',
+    short: 'Instruções e conselhos para solução de problemas e manutenção permanente. 0 - 12 meses.',
+    long: 'Finalmente! Um manual para principiantes sobre o \"funcionamento\" dos bebês.',
+    by: 'Borgenicht, Dr. Louis; Borgenicht, Joe',
+    images: [''],
+    price: 5,
+    status: null,
+    alias: 'lv_bebe_manual'
+  },
+  {
+    type: 'livros',
+    name: 'O Rei de Amarelo',
+    short: 'Livro de \"Terror Cósmico\".',
+    long: 'O Rei de Amarelo (The King in Yellow no original) é um livro de 1895 escrito por Robert W. Chambers e que consiste em uma coletânea de contos baseados em uma fictícia peça de teatro que enlouquece todos que a leem até o fim. Livro ainda lacrado.',
+    by: 'Chambers, Robert W.',
+    images: [''],
+    price: 10,
+    status: null,
+    alias: 'lv_rei_amarelo'
+  },
+
+  {
+    type: 'livros infantis',
+    name: 'Combine e Aprenda - números',
+    short: 'Livro infantil com alto-relevos.',
+    long: 'Este livrinho desafia as crianças a aprender novas palavras e números de uma forma divertida e única. As ilustrações em relevo se encaixam perfeitamente nas formas correspondentes na página seguinte, auxiliando os bebês e crianças mais novas a descobrir objetos e palavras de uma forma tátil nova. Um jeito perfeito de aprender e enriquecer o vocabulário com a temática: números. ',
+    by: 'Yoyo Books',
+    images: [''],
+    price: 3,
+    status: null,
+    alias:'lv_inf_combine_numeros'
+  },
+
+  {
+    type: 'esportes',
+    name: '2 Raquetes de Chutes',
+    short: 'Raquete de Chutes - América',
+    long: 'Ideal para treinamento de chutes e velocidade. Ótimo estado.',
+    images: [''],
+    price: 25,
+    status: null,
+    alias: 'es_raq_chutes'
+  },
+
+  {
+    type: 'livros',
+    name: 'Fotografia Digital',
+    short: 'Dicas e truques para tirar as melhores fotos',
+    long: 'Livro sobre fotografia que aborda como escolher a melhor câmera, técnicas de enquadramento de imagem e iluminação, etc...',
+    by: 'Gold Editora',
+    images: [''],
+    price: 10,
+    status: null,
+    alias: 'lv_fotog_digital'
+  },
+
+  {
+    type: 'livros',
+    name: 'Maquiagem Duda Molinos',
+    short: 'Livro sobre maquiagem.',
+    long: 'Esse livro traz informações indispensáveis a todos que buscam uma boa formação profissional na área ou que desejam valorizar ainda mais a sua aparência.',
+    by: 'Molinos, Duda',
+    images: [''],
+    price: 10,
+    status: null,
+    alias: 'lv_maquiagem_duda'
+  },
+  {
+    type: 'brinquedos',
+    name: 'tico-tico',
+    short: 'Triciclo',
+    long: 'Triciclo rosa e verde em ótimo estado.',
+    images: [''],
+    price: 20,
+    status: null,
+    alias: 'br_tico_tico'
+  },
+
+  {
+    type: 'infantil',
+    name: 'Troninho infantil',
+    short: 'Troninho infantil chicco em forma de patinho.',
+    long: 'Leve e facilmente transportável, com o centro removível para facilitar a limpeza.',
+    images: [''],
+    price: 15,
+    status: null,
+    alias: 'inf_troninho_duck'
+  },
+
+  {
+    type: 'livros',
+    name: 'Os intelectuais na Idade Média',
+    short: 'Editora José Olympio.',
+    long: '\“Capítulo fundamental da história medieval, este livro feito de limpidez, sutilezas e erudição apresenta ao leitor um autor fundamental, íntimo dos arquivos, hábil em criticar os documentos, ágil em interpretar seus silêncios, enérgico em revelar suas contradições. Passando com maestria da longa duração dos fatos pontuais, do material ao espiritual, Jacques Le Goff explica a história dos intelectuais medievais. Mas ele também confirma seu papel e seu lugar dentre os maiores intelectuais de nosso tempo.\” – Mary Del Priore.',
+    by: 'Le Goff, Jacques',
+    images: [''],
+    price: 5,
+    status: null,
+    alias: 'lv_jlg_intelect'
+  },
+
+  {
+    type: 'livros',
+    name: 'As raízes medievais da Europa',
+    short: 'Editora Vozes',
+    long: 'Jacques Le Goff, como é de costume dizer, enxerga além da visão padronizada que envolve nossos olhos e é desta forma que nos relata neste livro os acontecimentos mais importantes desde as ruínas do Império Romano até às descobertas do século XVI.',
+    by: 'Le Goff, Jacques',
+    images: [''],
+    price: 10,
+    status: null,
+    alias: 'lv_jlg_raizes'
+  },
+
+  {
+    type: 'esportes',
+    name: 'skate',
+    short: 'Skate traxart',
+    long: 'Skate roxo em ótimo estado.',
+    images: [''],
+    price: 50,
+    status: null,
+    alias: 'es_skate'
+  },
+
+
+
+
+
+
+]
