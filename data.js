af7001ff9e887be688ba8a50a1d285747c8e4165
Carol Arado <arado.carol@gmail.com>
2020-02-28 22:51:48 -0300

    Corrige dados e foto

--- data.js
+++ data.js
@@ -21,23 +21,23 @@ export default [
   },
 
   {
-    type: 'Livros',
+    type: 'livros',
     name: 'A Grande Enciclopédia Ilustrada das Plantas e Flores',
     short: 'Enciclopédia completa com 12 volumes.',
-    long: 'É a maior coleção de plantas ornamentais em língua portuguesa, dividida em 12 volumes, em 3.168 páginas e mais de 2.600 espécies.Biblioteca Natureza. Excelente estado com 2 volumes ainda lacrados.',
+    long: 'Nova esta enciclopédia vale 900 reais. É a maior coleção de plantas ornamentais em língua portuguesa, dividida em 12 volumes, em 3.168 páginas e mais de 2.600 espécies.Biblioteca Natureza. Excelente estado com 2 volumes ainda lacrados.',
     by: 'Romahn, Valerio.',
-    images: [''],
-    price: 250,
+    images: ['lv_encic_plantas.jpg'],
+    price: 300,
     status: null,
     alias: 'lv_encic_plantas'
   },
   {
-    type: 'Livros',
+    type: 'livros',
     name: 'bebê - manual do proprietário',
     short: 'Instruções e conselhos para solução de problemas e manutenção permanente. 0 - 12 meses.',
     long: 'Finalmente! Um manual para principiantes sobre o \"funcionamento\" dos bebês.',
     by: 'Borgenicht, Dr. Louis; Borgenicht, Joe',
-    images: [''],
+    images: ['lv_bb.jpg', 'lv_bb1.jpg'],
     price: 5,
     status: null,
     alias: 'lv_bebe_manual'
@@ -48,7 +48,7 @@ export default [
     short: 'Livro de \"Terror Cósmico\".',
     long: 'O Rei de Amarelo (The King in Yellow no original) é um livro de 1895 escrito por Robert W. Chambers e que consiste em uma coletânea de contos baseados em uma fictícia peça de teatro que enlouquece todos que a leem até o fim. Livro ainda lacrado.',
     by: 'Chambers, Robert W.',
-    images: [''],
+    images: ['lv_ra1.jpg','lv_ra2.jpg'],
     price: 10,
     status: null,
     alias: 'lv_rei_amarelo'
@@ -60,7 +60,7 @@ export default [
     short: 'Livro infantil com alto-relevos.',
     long: 'Este livrinho desafia as crianças a aprender novas palavras e números de uma forma divertida e única. As ilustrações em relevo se encaixam perfeitamente nas formas correspondentes na página seguinte, auxiliando os bebês e crianças mais novas a descobrir objetos e palavras de uma forma tátil nova. Um jeito perfeito de aprender e enriquecer o vocabulário com a temática: números. ',
     by: 'Yoyo Books',
-    images: [''],
+    images: ['lvi_num.jpg'],
     price: 3,
     status: null,
     alias:'lv_inf_combine_numeros'
@@ -71,7 +71,7 @@ export default [
     name: '2 Raquetes de Chutes',
     short: 'Raquete de Chutes - América',
     long: 'Ideal para treinamento de chutes e velocidade. Ótimo estado.',
-    images: [''],
+    images: ['es_raqchu.jpg'],
     price: 30,
     status: null,
     alias: 'es_raq_chutes'
@@ -83,7 +83,7 @@ export default [
     short: 'Dicas e truques para tirar as melhores fotos',
     long: 'Livro sobre fotografia que aborda como escolher a melhor câmera, técnicas de enquadramento de imagem e iluminação, etc...',
     by: 'Gold Editora',
-    images: [''],
+    images: ['lv_fotdig.jpg', 'lv_fotdig1.jpg'],
     price: 10,
     status: null,
     alias: 'lv_fotog_digital'
@@ -95,7 +95,7 @@ export default [
     short: 'Livro sobre maquiagem.',
     long: 'Esse livro traz informações indispensáveis a todos que buscam uma boa formação profissional na área ou que desejam valorizar ainda mais a sua aparência.',
     by: 'Molinos, Duda',
-    images: [''],
+    images: ['lv_duda.jpg'],
     price: 10,
     status: null,
     alias: 'lv_maquiagem_duda'
@@ -105,7 +105,7 @@ export default [
     name: 'tico-tico',
     short: 'Triciclo',
     long: 'Triciclo rosa e verde em ótimo estado.',
-    images: [''],
+    images: ['br_ticotico.jpg'],
     price: 20,
     status: null,
     alias: 'br_tico_tico'
@@ -116,7 +116,7 @@ export default [
     name: 'Troninho infantil',
     short: 'Troninho infantil chicco em forma de patinho.',
     long: 'Leve e facilmente transportável, com o centro removível para facilitar a limpeza. Carinhosamente conhecido como duck potty.',
-    images: [''],
+    images: ['in_duckpotty.jpg'],
     price: 15,
     status: null,
     alias: 'inf_troninho_duck'
@@ -128,7 +128,7 @@ export default [
     short: 'Editora José Olympio.',
     long: '\“Capítulo fundamental da história medieval, este livro feito de limpidez, sutilezas e erudição apresenta ao leitor um autor fundamental, íntimo dos arquivos, hábil em criticar os documentos, ágil em interpretar seus silêncios, enérgico em revelar suas contradições. Passando com maestria da longa duração dos fatos pontuais, do material ao espiritual, Jacques Le Goff explica a história dos intelectuais medievais. Mas ele também confirma seu papel e seu lugar dentre os maiores intelectuais de nosso tempo.\” – Mary Del Priore.',
     by: 'Le Goff, Jacques',
-    images: [''],
+    images: ['lv_int_idmed.jpg'],
     price: 5,
     status: null,
     alias: 'lv_jlg_intelect'
@@ -140,7 +140,7 @@ export default [
     short: 'Editora Vozes',
     long: 'Jacques Le Goff, como é de costume dizer, enxerga além da visão padronizada que envolve nossos olhos e é desta forma que nos relata neste livro os acontecimentos mais importantes desde as ruínas do Império Romano até às descobertas do século XVI.',
     by: 'Le Goff, Jacques',
-    images: [''],
+    images: ['lv_raizes_med_eur.jpg'],
     price: 10,
     status: null,
     alias: 'lv_jlg_raizes'
@@ -151,7 +151,7 @@ export default [
     name: 'skate',
     short: 'Skate traxart',
     long: 'Skate roxo em ótimo estado.',
-    images: [''],
+    images: ['es_skate.jpg', 'es_skate1.jpg'],
     price: 50,
     status: null,
     alias: 'es_skate'
@@ -162,7 +162,7 @@ export default [
     name: '2 Aparadores de soco',
     short: 'Aparadores de soco',
     long: '2 manoplas, aparadoras de soco da marca América em bom estado',
-    images: [''],
+    images: ['es_manopla1.jpg', 'es_manopla.jpg'],
     price: 20,
     status: null,
     alias: 'es_ap_soco'
@@ -173,7 +173,7 @@ export default [
     name: 'Rede de Voley',
     short: 'Rede de Voley',
     long: 'Rede para Voley da marca JMC nova.',
-    images: [''],
+    images: ['es_redevol.jpg'],
     price: 30,
     status: null,
     alias: 'es_rede_voley'
@@ -184,8 +184,8 @@ export default [
     name: 'Caloi Snake',
     short: 'Bicicleta caloi snake',
     long: 'Bicicleta caloi snake em bom estado. Vendida com cadeado com senha (dessa eu ainda me lembro!) e bolsinha no banco para pequenos objetos.',
-    images: [''],
-    price: 80,
+    images: ['es_snake.jpg'],
+    price: 110,
     status: null,
     alias: 'es_caloi_snake'
   },
@@ -195,7 +195,7 @@ export default [
     name: 'Caloi Aspen',
     short: 'Bicicleta caloi aspen',
     long: 'Bicicleta caloi aspen usada. Precisa trocar as rodas. Fora isso, bom estado. Tem cadeado com senha que vai junto mas a senha foi esquecida. Não cobramos extra pelo desafio.',
-    images: [''],
+    images: ['es_aspen.jpg'],
     price: 30,
     status: null,
     alias: 'es_caloi_aspen'
@@ -206,7 +206,7 @@ export default [
     name: 'Bola para pilates',
     short: 'Bola para pilates oxer',
     long: 'Bola para pilates da oxer em bom estado.',
-    images: [''],
+    images: ['es_pilates.jpg'],
     price: 10,
     status: null,
     alias: 'es_pilates'
@@ -217,7 +217,7 @@ export default [
     name: 'Carrinho de Bonecas',
     short: 'Carrinho de Bonecas - Baby Alive',
     long: 'Este carrinho é perfeito para a criança brincar de nanar e passear com o bebê. Com capota regulável, enquanto brincam desenvolvem a coordenação motora muito importante na fase de crescimento. O carrinho ainda conta com haste para a criança empurrar, com design robusto e material leve facilita o transportamento do carrinho com a boneca. Dimensões aproximadas do carrinho (altura x largura x comprimento): 64 x 31 x 44 cm',
-    images: [''],
+    images: ['br_carboneca.jpg', 'br_carboneca1.jpg'],
     price: 30,
     status: null,
     alias: 'br_carr_bonecas'
@@ -228,7 +228,7 @@ export default [
     name: 'Bike Dog',
     short: 'Cesta para andar de bicicleta com o seu cachorro ou com o cachorro de outra pessoa. Mas peça primeiro. Não roube cachorros.',
     long: 'A cadeirinha bike dog foi desenvolvida especialmente para cães de pequeno porte. Ideal para quem pretende fazer um passeio com seu cão ao ar livre. Em bom estado.',
-    images: [''],
+    images: ['es_bikedog1.jpg', 'es_bikedog.jpg'],
     price: 15,
     status: null,
     alias: 'es_bike_dog'
@@ -239,7 +239,7 @@ export default [
     name: 'Mesa com cadeira da Barbie',
     short: 'Mesinha com cadeira infantil',
     long: 'Espaço para armazenamento dentro da mesa (o tampo da mesa é removível). ',
-    images: [''],
+    images: ['in_mesa1.jpg', 'in_mesa.jpg', 'in_mesa2.jpg'],
     price: 30,
     status: null,
     alias: 'br_mesa_barbie'
@@ -250,7 +250,7 @@ export default [
     name: 'Grade para proteção de cama - Chicco',
     short: 'Protetor de cama',
     long: 'Seu sistema reclinável permite arrumar a cama e deitar o bebê sem que a grade precise ser removida, sendo fixada debaixo do colchão. A estrutura é feita em metal e tecido, e possui um bolso discreto para objetos. Pode ser utilizada em colchões com espessura máxima de 20 cm. Estrutura com fecho tipo compasso para se dobrar e guardar facilmente. Comprimento: 95 cm.',
-    images: [''],
+    images: ['in_gradeprot.jpg', 'in_gradeprot1.jpg'],
     price: 50,
     status: null,
     alias: 'inf_prot_cama'
@@ -260,7 +260,7 @@ export default [
     name: 'Andador do Mickey',
     short: 'Andador de empurrar',
     long: 'Com telefone de brinquedo. Tem botões com sons e luzes (não é alto demais mas pode ser desligado para paz de espírito dos adultos). Divertido e mais seguro que deixar a criança empurrando as cadeiras da mesa de jantar (acredite, ela vai tentar).',
-    images: [''],
+    images: ['br_andad.jpg', 'br_andad1.jpg'],
     price: 30,
     status: null,
     alias: 'br_andador'
@@ -281,7 +281,7 @@ export default [
     short: 'Descobrindo a Psicologia',
     long: 'Livro comprado para a matéria Introdução à Psicologia na faculdade. \"Esta obra reflete a crença de que a psicologia é um dos campos mais excitantes do conhecimento humano. Como nenhuma outra ciência, diz respeito à vida de todos de maneiras múltiplas e benéficas. Por isso um livro de psicologia pede e deve transmitir a relevância da pesquisa psicológica ao estudante de hoje. A psicologia é uma ciência incrivelmente diversa e dinâmica. Sem negligenciar os conceitos fundamentais, novos dados de pesquisas, abordagens e descobertas foram incorporados a praticamente todos os capítulos.\"',
     By: 'Hockenbury, Don H.; Hockenbury, Sandra E.',
-    images: [''],
+    images: ['lv_desc_psicol.jpg'],
     price: 20,
     status: null,
     alias: 'lv_desc_psicologia'
@@ -291,7 +291,7 @@ export default [
     name: 'Jardinagem Perfeita',
     short: 'Jardinagem Perfeita em até 30 minutos',
     long: 'Ideias criativas para vasos, canteiros e jardins e mais de 80 projetos práticos e rápidos. Livro em perfeito estado.',
-    images: [''],
+    images: ['lv_jard_perf.jpeg', 'lv_jard_perf1.jpeg'],
     price: 30,
     status: null,
     alias: 'lv_jardin_perfeita'
@@ -301,22 +301,22 @@ export default [
     name: 'Branca de Neve',
     short: 'Branca de Neve e os sete anões',
     long: 'Blu-ray duplo da Branca de Neve da Disney em perfeito estado.',
-    images: [''],
+    images: ['bl_b_d_neve.jpg'],
     price: 15,
     status: null,
     alias: 'bl_b_d_neve'
   },
 
-  {
-    type: 'brinquedos',
-    name: 'Mesa com cadeira da Barbie',
-    short: 'Mesinha com cadeira infantil',
-    long: 'Espaço para armazenamento dentro da mesa (o tampo da mesa é removível). ',
-    images: [''],
-    price: 30,
-    status: null,
-    alias: 'br_mesa_barbie'
-  },
+  // {
+  //   type: 'brinquedos',
+  //   name: 'Mesa com cadeira da Barbie',
+  //   short: 'Mesinha com cadeira infantil',
+  //   long: 'Espaço para armazenamento dentro da mesa (o tampo da mesa é removível). ',
+  //   images: [''],
+  //   price: 30,
+  //   status: null,
+  //   alias: 'br_mesa_barbie'
+  // },
 
 
 
